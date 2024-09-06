3595b20c4d0892e6b0381b7ff652b4be72a84cd7
Souvik Hazra <souvikhazra060@gmail.com>
2024-09-06 15:37:20 +0530

    Final Code
    ecommerce MERN stack

---
+++ routes/authRoute.js
@@ -0,0 +1,51 @@
+import express from 'express';
+import {registerController,loginController,testController, forgotPasswordController, updateProfileController, getOrdersController, getAllOrdersController, orderStatusController} from "../controllers/authController.js"
+import { isAdmin, requireSignIn } from '../middlewares/authMiddlewire.js';
+
+const router = express.Router()
+
+//routing
+//Register || method post
+
+router.post('/register', registerController)
+
+//Login || post
+
+router.post('/login',loginController)
+
+//Forget Passward || POST
+router.post('/forgot-password' , forgotPasswordController)
+
+//test route
+
+router.get('/test',requireSignIn,isAdmin,testController)
+
+//protected User route
+router.get('/user-auth', requireSignIn,(req,res)=>{
+    res.status(200).send({ok : true});
+})
+
+//protected Admin route
+router.get('/admin-auth', requireSignIn,isAdmin,(req,res)=>{
+    res.status(200).send({ok : true});
+})
+
+//update profile
+router.put("/profile", requireSignIn, updateProfileController);
+
+//orders
+router.get("/orders", requireSignIn, getOrdersController);
+
+//all orders
+router.get("/all-orders", requireSignIn, isAdmin, getAllOrdersController);
+
+// order status update
+router.put(
+    "/order-status/:orderId",
+    requireSignIn,
+    isAdmin,
+    orderStatusController
+  );
+
+
+export default router
